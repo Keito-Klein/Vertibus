b7c8cef8f3001be673f6f400b4aae710be57d597
keito-klein <taufikqillah25@gmail.com>
2023-02-17 11:58:22 +0700

    add feature: Monsters detail

--- sansekai.js
+++ sansekai.js
@@ -489,6 +489,39 @@ Membuat sticker dengan teks
   })
   break;
 
+  case 'mobs':
+case 'boss':
+  if(!text) return reply("Masukan nama boss/monster!")
+
+  axios.get(`https://coryn.club/monster.php?name=${text}#`)
+ .then((response) => {
+    if(response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html)
+        array = []
+         $(".card-container > div").each(function(i, elem) {
+            array[i] = {
+                boss: $(this).find("div > .card-title-inverse").text().trim(),
+                diff: $(this).find(".monster-no-pic > div > .item-prop > div:nth-child(2) > p:nth-child(2)").text().trim(),
+                lv: $(this).find(".monster-no-pic > div > .item-prop > div:nth-child(1) > p:nth-child(2)").text().trim(),
+                hp: $(this).find(".monster-no-pic > div > .item-prop > div:nth-child(3) > p:nth-child(2)").text().trim(),
+                exp: $(this).find(".monster-no-pic > div > .item-prop > div:nth-child(5) > p:nth-child(2)").text().trim(),
+                element: $(this).find(".monster-no-pic > div > .item-prop > div:nth-child(4) > p:nth-child(2)").text().trim(),
+                tamable: $(this).find(".monster-no-pic > div > .item-prop > div:nth-child(6) > p:nth-child(2)").text().trim(),
+                map: $(this).find(".item-prop > div:nth-child(2) > a").text().trim(),
+                drop: $(this).find(`.monster-drop > div > a`).text().trim()
+            }
+        })
+            db = `Berikut detail dari ${command} bernama ${text}\n\n`
+            for (let i = 0; i < array.length; i++) {
+              db += `-----------------------------------\nBoss: ${array[i].boss}\nDiff: ${array[i].diff}\nLevel: ${array[i].lv}\nHP: ${array[i].hp}\nEXP: ${array[i].exp}\nElement: ${array[i].element}\nTamable: ${array[i].tamable}\nLocation: ${array[i].map}\nDrop: ${array[i].drop}\n`
+            }
+            client.sendText(from, db, mek)
+        }
+    
+ })
+  break
+
   case 'farm':
   case 'farming':
   if (!text) return reply("Mau farming apa?\n\n/logam\n/kayu\n/fauna\n/obat\n/kain _(Comming Soon)_\n/lainnya _(Comming Soon)_")
@@ -702,6 +735,9 @@ case "buff":
   db += `\nJika ada perubahan/mau ditambahkan, tag admin/pengurus guild🙏👍`
   client.sendText(from, db, mek)
   break
+
+
+
 
   case 'meta':
     se = client.groupMetadata()
