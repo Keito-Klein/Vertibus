84ab8e0d9a63cdef7f754824b59a0fb65cdbbef0
keito-klein <taufikqillah25@gmail.com>
2024-04-03 19:13:25 +0700

    change guild feature to public feature

--- sansekai.js
+++ sansekai.js
@@ -124,70 +124,6 @@ const delBuff = (name) => {
   }
 }
 
-async function mp4ToWebp(file, stickerMetadata) {
-  if (stickerMetadata) {
-    if (!stickerMetadata.pack) stickerMetadata.pack = '‎'
-    if (!stickerMetadata.author) stickerMetadata.author = '‎'
-    if (!stickerMetadata.crop) stickerMetadata.crop = false
-  } else if (!stickerMetadata) {
-    stickerMetadata = { pack: '‎', author: '‎', crop: false }
-  }
-  let getBase64 = file.toString('base64')
-  const Format = {
-    file: `data:video/mp4;base64,${getBase64}`,
-    processOptions: {
-      crop: stickerMetadata?.crop,
-      startTime: '00:00:00.0',
-      endTime: '00:00:7.0',
-      loop: 0
-    },
-    stickerMetadata: {
-      ...stickerMetadata
-    },
-    sessionInfo: {
-      WA_VERSION: '2.2106.5',
-      PAGE_UA: 'WhatsApp/2.2037.6 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
-      WA_AUTOMATE_VERSION: '3.6.10 UPDATE AVAILABLE: 3.6.11',
-      BROWSER_VERSION: 'HeadlessChrome/88.0.4324.190',
-      OS: 'Windows Server 2016',
-      START_TS: 1614310326309,
-      NUM: '6247',
-      LAUNCH_TIME_MS: 7934,
-      PHONE_VERSION: '2.20.205.16'
-    },
-    config: {
-      sessionId: 'session',
-      headless: true,
-      qrTimeout: 20,
-      authTimeout: 0,
-      cacheEnabled: false,
-      useChrome: true,
-      killProcessOnBrowserClose: true,
-      throwErrorOnTosBlock: false,
-      chromiumArgs: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--aggressive-cache-discard',
-        '--disable-cache',
-        '--disable-application-cache',
-        '--disable-offline-load-stale-cache',
-        '--disk-cache-size=0'
-      ],
-      executablePath: 'C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe',
-      skipBrokenMethodsCheck: true,
-      stickerServerEndpoint: true
-     }
-  }
-  let res = await fetch('https://sticker-api.openwa.dev/convertMp4BufferToWebpDataUrl', {
-    method: 'post',
-    headers: {
-      Accept: 'application/json, text/plain, /',
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(Format)
-  })
-  return Buffer.from((await res.text()).split(';base64,')[1], 'base64')
-}
 
 
 module.exports = sansekai = async (client, m, chatUpdate, store) => {
@@ -445,22 +381,6 @@ module.exports = sansekai = async (client, m, chatUpdate, store) => {
   return `${Math.floor(Math.random() * 10000)}${ext}`
 }
 
-    let yhh = ['malas', 'malas banget', '🧢', '🗿🗿', 'apsi jir😜']
-  //if(isCmd2 && sender == '6289515792657@s.whatsapp.net') return reply(yhh[Math.floor(Math.random())])
-
-    // BUG Solver
-    if (budy.includes('wa.me/settings')) {
-      //gg = 628980354156-1628522503@g.us
-
-      if (botAdmin) {
-      await client.sendMessage(from, { delete: m.key })  
-      }
-      if (!botAdmin) {
-        for (let amount = 0; amount < 26; amount++) {
-          await client.sendText(from, 'Someone sending a bug')
-        }
-      }
-    }
 
     //maintenance Toggle
     if (global.maintenance === true && isCmd2 && !isOwner) {
@@ -954,7 +874,6 @@ case 'code':
 case 'adres':
 case 'addres':
 case 'adress':
-  if(!isMyGuild) return reply(lang.onGuild())
     reply(lang.buff(q))
   break
 
